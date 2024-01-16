0390fea6a06820ce283f5dd4e5f1aab87b2ff7a4
rohansahu700@gmail.com <rohansahu700@gmail.com>
2024-01-16 06:52:03 +0530

    update main.js

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const width = canvas.width;
 const height = canvas.height;
 const totalParticle = 100;
 const constG = 6.67428e-11   //m³kg-¹s-²
+const velCap = 200;
+const stifness = 5; //perfect bounce(1)->(increase) bounce less
+const trailLength = 9; 
 
 function vector2(x,y) {
     this.x = x || 0;
@@ -18,9 +21,17 @@ function particle(velocity,position,mass){
 }
 
 let ball = new Array(totalParticle);
+let trailData = new Array(trailLength);
 
 for (let i = 0; i < totalParticle; i++) {
     ball[i] = new particle();
+}
+
+for(let i=0; i<trailLength; i++){
+    trailData[i] = new Array(totalParticle);
+    for(let j = 0; j<totalParticle; j++){
+        trailData[i][j] = new vector2();
+    }
 }
 //initialiser
 for(let i=0; i<totalParticle; i++){
@@ -28,7 +39,12 @@ for(let i=0; i<totalParticle; i++){
                             Math.floor(Math.random() * (height)));
     ball[i].velocity = new vector2(0,0);
     ball[i].mass = 1;  //Math.floor(Math.random()*2);
+    for(let j = 0; j<trailLength; j++){
+        trailData[j][i].x =ball[i].position.x;
+        trailData[j][i].y =ball[i].position.y;
+    }
 }
+
 // Handle the touch event
 function handleTouchStart(event) {
     event.preventDefault();
@@ -46,9 +62,8 @@ let dtime;
 
 let planet = new particle();
 planet.position = new vector2(width/2,height/2);
-planet.mass = 14e16;
+planet.mass = 14e14;
 
-const velCap = 200;
 //runtime function
 function render(){
     //delta time calculation
@@ -77,17 +92,22 @@ function render(){
         ball[i].velocity = new vector2(ball[i].velocity.x + (xforce/ball[i].mass)*(dtime/1000),ball[i].velocity.y + (yforce/ball[i].mass)*(dtime/1000));
         ball[i].position = new vector2((dtime/1000)*ball[i].velocity.x+ball[i].position.x,(dtime/1000)*ball[i].velocity.y+ball[i].position.y);
         
+        //celocity cap
         ball[i].velocity.x = Math.min(velCap,Math.max(ball[i].velocity.x,-velCap));
         ball[i].velocity.y = Math.min(velCap,Math.max(ball[i].velocity.y,-velCap));
         
         //boundry wall
         ball[i].position.x = Math.min(width,Math.max(ball[i].position.x,0));
-        ball[i].velocity.x = (ball[i].position.x==0 || ball[i].position.x==width)? 0:ball[i].velocity.x;
         ball[i].position.y = Math.min(height,Math.max(ball[i].position.y,0));
-        ball[i].velocity.y = (ball[i].position.y==0 || ball[i].position.y==height)? 0:ball[i].velocity.y;
+        //bouncefromwall
+        if(ball[i].position.x<=0 || ball[i].position.x>=width){
+            ball[i].velocity.x = (ball[i].velocity.x*-1)/stifness;
+        }
+        if(ball[i].position.y<=0 || ball[i].position.y>=height){
+            ball[i].velocity.y = (ball[i].velocity.y*-1)/stifness;
+        }
         //console.log(ball[i].velocity.x);
     }
-    
     //rendring
     ctx.clearRect(0,0,width,height);
     ctx.beginPath();
@@ -95,13 +115,32 @@ function render(){
     ctx.arc(planet.position.x,planet.position.y,6,0,2*Math.PI
     );
     ctx.fill();
-    ctx.fillStyle = "red";
-    for(let j=0; j<totalParticle; j++){
+    
+    for(let i=0; i<totalParticle; i++){
+        ctx.fillStyle = "red";
         ctx.beginPath();
-        ctx.arc(ball[j].position.x,ball[j].position.y,2,0,2*Math.PI);
+        ctx.arc(ball[i].position.x,ball[i].position.y,3,0,2*Math.PI);
         ctx.fill();
+        for(let j=0; j<trailLength; j++){
+            ctx.fillStyle = "#fff00"
+            ctx.beginPath();
+            ctx.arc(trailData[j][i].x,trailData[j][i].y,1,0,2*Math.PI);
+            ctx.fill();
+        }
     }
     
+    //moves the 2nd last trail postition to last and so on
+    for (let i=0; i<totalParticle; i++){
+        for(let j=trailLength-1; j>0; j--){
+            trailData[j][i].x = trailData[j-1][i].x;  
+            trailData[j][i].y = trailData[j-1][i].y;
+        }
+    }
+    //the 1st most trail position filled with current position
+    for (let i=0; i<totalParticle; i++){
+        trailData[0][i].x = ball[i].position.x;
+        trailData[0][i].y = ball[i].position.y;
+    }
     requestAnimationFrame(render);
 }
 //starts the animation
